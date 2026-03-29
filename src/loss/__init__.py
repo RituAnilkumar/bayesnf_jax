27a6c41dc6f05c2951846f662dbdac94c5c217ff
ritu <ritu.anilkumar@gmail.com>
2026-03-29 23:09:16 +0100

    fix elbo calls

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,3 +1,3 @@
-from .elbo import compute_elbo, make_beta_schedule
+from .elbo import pretrain_elbo, finetune_elbo, make_beta_schedule
 from .likelihood import oggm_loss, hugonnet_loss, glambie_loss
 from .aggregation import segment_mean, glacier_annual_mean, MM_TO_MWE, KGM2_TO_MWE
