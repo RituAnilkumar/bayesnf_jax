4d54ae368f9e5c40d4d50d003403be65d52a2a49
ritu <ritu.anilkumar@gmail.com>
2026-03-29 23:19:31 +0100

    fix temporal_avg calls in init

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,3 +1,3 @@
 from .elbo import pretrain_elbo, finetune_elbo, make_beta_schedule
-from .likelihood import oggm_loss, hugonnet_loss, glambie_loss
+from .likelihood import oggm_loss, temporal_avg_loss, glambie_loss
 from .aggregation import segment_mean, glacier_annual_mean, MM_TO_MWE, KGM2_TO_MWE
